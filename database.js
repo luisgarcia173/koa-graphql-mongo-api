6bf464e519bde26ed4038824feb8da101a3adfff
Luis Garcia <luisgarcia173@gmail.com>
2020-04-30 11:48:34 -0300

    first commit

---
+++ database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const initDB = () => {
+
+  mongoose.connect(
+    'mongodb://localhost:27017/koa-graphql',
+    { useNewUrlParser: true }
+  );
+
+  mongoose.connection.once('open', () => {
+    console.log('connected to database');
+  });
+
+}
+
+module.exports = initDB;
